2deee8b08e5245d753ef316e7a6bee406f889409
heui-yong <gusrnr9878@gmail.com>
2024-01-18 10:36:58 +0900

    end patterns_codelab project

--- patterns_codelab/lib/data.dart
+++ patterns_codelab/lib/data.dart
@@ -3,13 +3,88 @@ import 'dart:convert';
 class Document {
   final Map<String, Object?> _json;
   Document() : _json = jsonDecode(documentJson);
+
+  (String, {DateTime modified}) getMetadata1() {
+    var tittle = "My Document > Heui Yong!";
+    var now = DateTime.now();
+
+    return (tittle, modified: now);
+  }
+
+  (String, {DateTime modified}) getMetadata2() {
+    if (_json.containsKey('metadata')) {
+      var metadataJson = _json['metadata'];
+
+      if (metadataJson is Map) {
+        var tittle = "${metadataJson['title'] as String} > Read JSON values without patterns";
+        var localModified = DateTime.parse(metadataJson['modified'] as String);
+
+        return (tittle, modified: localModified);
+      }
+    }
+    throw const FormatException('Unexpected JSON');
+  }
+
+  (String, {DateTime modified}) getMetadata3() {
+    if (_json
+        case{
+          'metadata': {
+            'title': String title,
+            'modified': String localModified,
+          }
+        }) {
+      return ("$title > Read JSON values using a map pattern", modified: DateTime.parse(localModified));
+    } else {
+      throw const FormatException('Unexpected JSON');
+    }
+  }
+
+  List<Block> getBlock() {
+    if (_json case {'blocks' : List blocksJson}) {
+      return <Block>[
+        for (var blockJson in blocksJson) Block.fromJson(blockJson)
+      ];
+    } else {
+      throw const FormatException('Unexpected JSON');
+    }
+  }
+}
+
+sealed class Block {
+  Block();
+
+  factory Block.fromJson(Map<String, Object?> json) {
+    return switch (json) {
+      {'type': 'h1', 'text': String text} => HeaderBlock(text),
+      {'type': 'p', 'text': String text} => ParagraphBlock(text),
+      {'type': 'checkbox', 'text': String text, 'checked': bool checked} =>
+          CheckboxBlock(text, checked),
+      _ => throw const FormatException('Unexpected JSON format'),
+    };
+  }
+}
+
+class HeaderBlock extends Block {
+  final String text;
+  HeaderBlock(this.text);
+}
+
+class ParagraphBlock extends Block {
+  final String text;
+  ParagraphBlock(this.text);
+}
+
+class CheckboxBlock extends Block {
+  final String text;
+  final bool isChecked;
+  CheckboxBlock(this.text, this.isChecked);
 }
 
 const documentJson = '''
 {
   "metadata": {
     "title": "My Document",
-    "modified": "2023-05-10"
+    "modified": "2024-01-18"
   },
   "blocks": [
     {
@@ -22,7 +97,7 @@ const documentJson = '''
     },
     {
       "type": "checkbox",
-      "checked": false,
+      "checked": true,
       "text": "Learn Dart 3"
     }
   ]
--- patterns_codelab/lib/main.dart
+++ patterns_codelab/lib/main.dart
@@ -30,17 +30,71 @@ class DocumentScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var (tittle, :modified) = document.getMetadata3();
+    var blocks = document.getBlock();
+    var formattedModifiedDate = formatDate(modified);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Title goes here'),
+        title: Text(tittle),
       ),
       body: Column(
         children: [
-          Center(
-            child: Text('Body goes here'),
-          ),
+          Text('Last modified: $formattedModifiedDate'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: blocks.length,
+              itemBuilder: (context, index) {
+                return BlockWidget(block: blocks[index]);
+              },
+            ),
+          )
         ],
       ),
     );
   }
+}
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({
+    required this.block,
+    Key? key
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: switch (block) {
+        HeaderBlock(:var text) => Text(
+          text,
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+        ParagraphBlock(:var text) => Text(text),
+        CheckboxBlock(:var text, :var isChecked) => Row(
+          children: [
+            Checkbox(value: isChecked, onChanged: (_) {}),
+            Text(text)
+          ],
+        ),
+      }
+    );
+  }
+}
+
+String formatDate(DateTime dateTime) {
+  var today = DateTime.now();
+  var difference = dateTime.difference(today);
+
+  return switch(difference) {
+    Duration(inDays: 0) => 'today',
+    Duration(inDays: 1) => 'tomorrow',
+    Duration(inDays: -1) => 'yesterday',
+    Duration(inDays: var days) when days > 7 => '${days ~/ 7} weeks from now',
+    Duration(inDays: var days) when days < -7 => '${days.abs() ~/ 7} weeks ago',
+    Duration(inDays: var days, isNegative: true) => '${days.abs()} days ago',
+    Duration(inDays: var days) => '$days days from now',
+  };
 }
