710b4def70caf004db8422e196f0a33ba860b8a8
heui-yong <gusrnr9878@gmail.com>
2024-01-18 15:08:08 +0900

    end inherited_widget project

--- inherited_widget/lib/main.dart
+++ inherited_widget/lib/main.dart
@@ -1,18 +1,111 @@
 import 'package:flutter/material.dart';
 
-final GlobalKey<ShoppingCartIconState> shoppingCart =
-GlobalKey<ShoppingCartIconState>();
-final GlobalKey<ProductListWidgetState> productList =
-GlobalKey<ProductListWidgetState>();
-
 void main() {
   runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Store',
-      home: MyStorePage(),
+    const AppStateWidget(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Store',
+        home: MyStorePage(),
+      ),
     ),
   );
+}
+
+class AppState {
+  AppState({
+    required this.productList,
+    this.itemsInCart = const <String>{},
+  });
+
+  final List<String> productList;
+  final Set<String> itemsInCart;
+
+  AppState copyWith({
+    List<String>? productList,
+    Set<String>? itemsInCart,
+  }) {
+    return AppState(
+      productList: productList ?? this.productList,
+      itemsInCart: itemsInCart ?? this.itemsInCart,
+    );
+  }
+}
+
+class AppStateScope extends InheritedWidget {
+  const AppStateScope(this.data, {super.key, required super.child});
+
+  final AppState data;
+
+  static AppState of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<AppStateScope>()!.data;
+  }
+
+  @override
+  bool updateShouldNotify(AppStateScope oldWidget) {
+    return data != oldWidget.data;
+  }
+}
+
+class AppStateWidget extends StatefulWidget {
+  const AppStateWidget({required this.child, super.key});
+
+  final Widget child;
+
+  static AppStateWidgetState of(BuildContext context) {
+    return context.findAncestorStateOfType<AppStateWidgetState>()!;
+  }
+
+  @override
+  AppStateWidgetState createState() => AppStateWidgetState();
+}
+
+class AppStateWidgetState extends State<AppStateWidget> {
+  AppState _data = AppState(
+    productList: Server.getProductList(),
+  );
+
+  void setProductList(List<String> newProductList) {
+    if (newProductList != _data.productList) {
+      setState(() {
+        _data = _data.copyWith(
+          productList: newProductList,
+        );
+      });
+    }
+  }
+
+  void addToCart(String id) {
+    if (!_data.itemsInCart.contains(id)) {
+      final Set<String> newItemsInCart = Set<String>.from(_data.itemsInCart);
+      newItemsInCart.add(id);
+      setState(() {
+        _data = _data.copyWith(
+          itemsInCart: newItemsInCart,
+        );
+      });
+    }
+  }
+
+  void removeFromCart(String id) {
+    if (_data.itemsInCart.contains(id)) {
+      final Set<String> newItemsInCart = Set<String>.from(_data.itemsInCart);
+      newItemsInCart.remove(id);
+      setState(() {
+        _data = _data.copyWith(
+          itemsInCart: newItemsInCart,
+        );
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppStateScope(
+      _data,
+      child: widget.child,
+    );
+  }
 }
 
 class MyStorePage extends StatefulWidget {
@@ -31,16 +124,14 @@ class MyStorePageState extends State<MyStorePage> {
     setState(() {
       _inSearch = !_inSearch;
     });
-
     _controller.clear();
-    productList.currentState!.productList = Server.getProductList();
+    AppStateWidget.of(context).setProductList(Server.getProductList());
   }
 
   void _handleSearch() {
     _focusNode.unfocus();
     final String filter = _controller.text;
-    productList.currentState!.productList =
-        Server.getProductList(filter: filter);
+    AppStateWidget.of(context).setProductList(Server.getProductList(filter: filter));
   }
 
   @override
@@ -62,11 +153,13 @@ class MyStorePageState extends State<MyStorePage> {
               decoration: InputDecoration(
                 hintText: 'Search Google Store',
                 prefixIcon: IconButton(
-                    icon: const Icon(Icons.search),
-                    onPressed: _handleSearch),
+                  icon: const Icon(Icons.search),
+                  onPressed: _handleSearch,
+                ),
                 suffixIcon: IconButton(
-                    icon: const Icon(Icons.close),
-                    onPressed: _toggleSearch),
+                  icon: const Icon(Icons.close),
+                  onPressed: _toggleSearch,
+                ),
               ),
             )
                 : null,
@@ -76,13 +169,13 @@ class MyStorePageState extends State<MyStorePage> {
                   onPressed: _toggleSearch,
                   icon: const Icon(Icons.search, color: Colors.black),
                 ),
-              ShoppingCartIcon(key: shoppingCart),
+              const ShoppingCartIcon(),
             ],
             backgroundColor: Colors.white,
             pinned: true,
           ),
-          SliverToBoxAdapter(
-            child: ProductListWidget(key: productList),
+          const SliverToBoxAdapter(
+            child: ProductListWidget(),
           ),
         ],
       ),
@@ -90,25 +183,12 @@ class MyStorePageState extends State<MyStorePage> {
   }
 }
 
-class ShoppingCartIcon extends StatefulWidget {
+class ShoppingCartIcon extends StatelessWidget {
   const ShoppingCartIcon({super.key});
 
   @override
-  ShoppingCartIconState createState() => ShoppingCartIconState();
-}
-
-class ShoppingCartIconState extends State<ShoppingCartIcon> {
-  Set<String> get itemsInCart => _itemsInCart;
-  Set<String> _itemsInCart = <String>{};
-
-  set itemsInCart(Set<String> value) {
-    setState(() {
-      _itemsInCart = value;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final Set<String> itemsInCart = AppStateScope.of(context).itemsInCart;
     final bool hasPurchase = itemsInCart.isNotEmpty;
     return Stack(
       alignment: Alignment.center,
@@ -141,55 +221,32 @@ class ShoppingCartIconState extends State<ShoppingCartIcon> {
   }
 }
 
-class ProductListWidget extends StatefulWidget {
+class ProductListWidget extends StatelessWidget {
   const ProductListWidget({super.key});
 
-  @override
-  ProductListWidgetState createState() => ProductListWidgetState();
-}
-
-class ProductListWidgetState extends State<ProductListWidget> {
-  List<String> get productList => _productList;
-  List<String> _productList = Server.getProductList();
-
-  set productList(List<String> value) {
-    setState(() {
-      _productList = value;
-    });
+  void _handleAddToCart(String id, BuildContext context) {
+    AppStateWidget.of(context).addToCart(id);
   }
 
-  Set<String> get itemsInCart => _itemsInCart;
-  Set<String> _itemsInCart = <String>{};
-
-  set itemsInCart(Set<String> value) {
-    setState(() {
-      _itemsInCart = value;
-    });
+  void _handleRemoveFromCart(String id, BuildContext context) {
+    AppStateWidget.of(context).removeFromCart(id);
   }
 
-  void _handleAddToCart(String id) {
-    itemsInCart = _itemsInCart..add(id);
-    shoppingCart.currentState!.itemsInCart = itemsInCart;
-  }
-
-  void _handleRemoveFromCart(String id) {
-    itemsInCart = _itemsInCart..remove(id);
-    shoppingCart.currentState!.itemsInCart = itemsInCart;
-  }
-
-  Widget _buildProductTile(String id) {
+  Widget _buildProductTile(String id, BuildContext context) {
     return ProductTile(
       product: Server.getProductById(id),
-      purchased: itemsInCart.contains(id),
-      onAddToCart: () => _handleAddToCart(id),
-      onRemoveFromCart: () => _handleRemoveFromCart(id),
+      purchased: AppStateScope.of(context).itemsInCart.contains(id),
+      onAddToCart: () => _handleAddToCart(id, context),
+      onRemoveFromCart: () => _handleRemoveFromCart(id, context),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    final List<String> productList = AppStateScope.of(context).productList;
     return Column(
-      children: productList.map(_buildProductTile).toList(),
+      children:
+      productList.map((id) => _buildProductTile(id, context)).toList(),
     );
   }
 }
@@ -260,6 +317,7 @@ class ProductTile extends StatelessWidget {
     );
   }
 }
+
 // The code below is for the dummy server, and you should not need to modify it
 // in this workshop.
 
