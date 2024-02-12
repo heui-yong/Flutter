2d0830973c70091ddb9f98213cacd94f95aceb22
heui-yong <gusrnr9878@gmail.com>
2024-02-13 08:53:29 +0900

    userInfoDetail Json save

--- dog_walking_app/lib/model/user_info_detail_model.dart
+++ dog_walking_app/lib/model/user_info_detail_model.dart
@@ -3,5 +3,22 @@ class UserInfoDetailModel{
 }
 
 class UerInfoDetail{
+  String name;
+  String distance;
+  String cost;
+  String imageUrl;
+  String scope;
+  String walks;
+}
 
+class About {
+  String age;
+  String experience;
+  String description;
+
+  About({
+    required this.age,
+    required this.experience,
+    required this.description
+  });
 }
